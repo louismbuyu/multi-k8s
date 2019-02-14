e045e18e410cdb21d011ae56248f8142eaad9a76
Louis Mbuyu <louismbuyu17@gmail.com>
2019-02-13 22:33:02 -0600

    added more deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t louismbuyu17/multi-client:latest -t louismbuyu17/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t louismbuyu17/multi-server:latest -t louismbuyu17/multi-server:$SHA -f ./client/Dockerfile ./server
+docker build -t louismbuyu17/multi-worker:latest -t louismbuyu17/multi-worker:$SHA -f ./client/Dockerfile ./worker
+
+docker push louismbuyu17/multi-client:latest
+docker push louismbuyu17/multi-server:latest
+docker push louismbuyu17/multi-worker:latest
+
+docker push louismbuyu17/multi-client:$SHA
+docker push louismbuyu17/multi-server:$SHA
+docker push louismbuyu17/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=louismbuyu17/multi-server:$SHA
+kubectl set image deployments/client-deployment client=louismbuyu17/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=louismbuyu17/multi-worker:$SHA
