c9efb29b31051bda2d63feec6fbf2d5ac1b9018c
Louis Mbuyu <louismbuyu17@gmail.com>
2019-02-14 21:47:31 -0600

    render please

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    console.log("State: ",this.state);
+    return this.state.seenIndexes && this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
